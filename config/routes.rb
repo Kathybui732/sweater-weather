a31e4f6f88980b33d585683c534b066a916c76d1
Kathy Bui <kathy.bui1992@gmail.com>
2020-11-03 23:56:54 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get '/', to: 'welcome#index'
       get '/forecast', to: 'forecast#show'
       get '/backgrounds', to: 'backgrounds#show'
       post '/sessions', to: 'sessions#create'
@@ -8,4 +9,6 @@ Rails.application.routes.draw do
       post '/road_trip', to: 'road_trip#create'
     end
   end
+
+  get '/', to: redirect('/api/v1')
 end
